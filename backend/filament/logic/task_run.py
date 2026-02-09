1400f06466d4a927e2319302f7a376125f292e1c
jameswu1991 <james@centauri-ai.tech>
2026-02-09 12:02:05 -0800

    await session delete

--- backend/filament/logic/task_run.py
+++ backend/filament/logic/task_run.py
@@ -1,8 +1,12 @@
+from beartype import beartype
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from filament.db_models import TaskRun, TaskState
 from filament.task_state import transition_state
 
 
-async def cancel_task_run(session, task_run: TaskRun):
+@beartype
+async def cancel_task_run(session: AsyncSession, task_run: TaskRun):
     if task_run.state in TaskState.TERMINAL:
         return task_run
     await transition_state(session, task_run.task_uuid, TaskState.CANCELLED)
@@ -10,10 +14,11 @@ async def cancel_task_run(session, task_run: TaskRun):
         await cancel_task_run(session, child_task_run)
 
 
-async def delete_task_run(session, task_run: TaskRun):
+@beartype
+async def delete_task_run(session: AsyncSession, task_run: TaskRun):
     for child_task_run in await task_run.awaitable_attrs.child_tasks:
         await delete_task_run(session, child_task_run)
     for task_run_state_transition in await task_run.awaitable_attrs.state_transitions:
-        session.delete(task_run_state_transition)
-    session.delete(task_run)
-    session.flush()
+        await session.delete(task_run_state_transition)
+    await session.delete(task_run)
+    await session.flush()
