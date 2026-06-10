add6c75624834ac5b3ab7f9e62811f7babd2c12f
jameswu1991 <james@centauri-ai.tech>
2026-06-09 18:26:02 -0700

    rename cache prefix

--- src/filament/logic/cache_utils.py
+++ src/filament/logic/cache_utils.py
@@ -3,21 +3,21 @@ import pickle
 
 from filament.redis.client import r
 
-CACHE_KEY_PREFIX = 'cache:'
+CACHE_KEY_PREFIX = 'filament:cache:'
 
 DEFAULT_TTL = 60 * 60 * 24
 
 
-def get_key(key):
+def get_cache_key(key):
     return f'{CACHE_KEY_PREFIX}{key}'
 
 
 async def cache_has_key(key):
-    return await r.exists(get_key(key))
+    return await r.exists(get_cache_key(key))
 
 
 async def cache_get(key):
-    base64_encoded_value = await r.get(get_key(key))
+    base64_encoded_value = await r.get(get_cache_key(key))
     if base64_encoded_value is None:
         return None
     pickled_value = base64.b64decode(base64_encoded_value)
@@ -29,4 +29,4 @@ async def cache_set(key, value, ttl=None):
         ttl = DEFAULT_TTL
     pickled_value = pickle.dumps(value)
     base64_encoded_value = base64.b64encode(pickled_value).decode('utf-8')
-    await r.set(get_key(key), base64_encoded_value, ex=ttl)
+    await r.set(get_cache_key(key), base64_encoded_value, ex=ttl)
